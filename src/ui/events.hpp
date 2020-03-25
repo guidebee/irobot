c72830763f1c437f84bbc98e49630de8c4895e78
James Shen <james.shen@guidebee.com>
2020-03-25 11:00:51 +0800

    add decoder.cpp

---
+++ src/ui/events.hpp
@@ -0,0 +1,13 @@
+//
+// Created by James Shen on 25/3/20.
+// Copyright (c) 2020 GUIDEBEE IT. All rights reserved
+//
+
+#ifndef ANDROID_IROBOT_EVENTS_HPP
+#define ANDROID_IROBOT_EVENTS_HPP
+
+#define EVENT_NEW_SESSION SDL_USEREVENT
+#define EVENT_NEW_FRAME (SDL_USEREVENT + 1)
+#define EVENT_STREAM_STOPPED (SDL_USEREVENT + 2)
+
+#endif //ANDROID_IROBOT_EVENTS_HPP
---
+++ src/video/decoder.hpp
@@ -0,0 +1,43 @@
+//
+// Created by James Shen on 25/3/20.
+// Copyright (c) 2020 GUIDEBEE IT. All rights reserved
+//
+
+#ifndef ANDROID_IROBOT_DECODER_HPP
+#define ANDROID_IROBOT_DECODER_HPP
+
+#if defined (__cplusplus)
+extern "C" {
+#endif
+
+#include <stdbool.h>
+#include <libavformat/avformat.h>
+
+#if defined (__cplusplus)
+}
+#endif
+
+#include "config.hpp"
+
+struct video_buffer;
+
+struct decoder {
+    struct video_buffer *video_buffer;
+    AVCodecContext *codec_ctx;
+};
+
+void
+decoder_init(struct decoder *decoder, struct video_buffer *vb);
+
+bool
+decoder_open(struct decoder *decoder, const AVCodec *codec);
+
+void
+decoder_close(struct decoder *decoder);
+
+bool
+decoder_push(struct decoder *decoder, const AVPacket *packet);
+
+void
+decoder_interrupt(struct decoder *decoder);
+#endif //ANDROID_IROBOT_DECODER_HPP
