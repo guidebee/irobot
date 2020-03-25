f370d6be148006a70f6b4a13fc32151dd99738e3
James Shen <james.shen@guidebee.com>
2020-03-25 10:37:56 +0800

    add receiver.cpp

---
+++ src/android/receiver.hpp
@@ -0,0 +1,49 @@
+//
+// Created by James Shen on 25/3/20.
+// Copyright (c) 2020 GUIDEBEE IT. All rights reserved
+//
+
+#ifndef ANDROID_IROBOT_RECEIVER_HPP
+#define ANDROID_IROBOT_RECEIVER_HPP
+
+#if defined (__cplusplus)
+extern "C" {
+#endif
+
+
+#include <stdbool.h>
+#include <SDL2/SDL_mutex.h>
+#include <SDL2/SDL_thread.h>
+
+#if defined (__cplusplus)
+}
+#endif
+
+
+#include "config.hpp"
+#include "util/net.hpp"
+
+
+// receive events from the device
+// managed by the controller
+struct receiver {
+    socket_t control_socket;
+    SDL_Thread *thread;
+    SDL_mutex *mutex;
+};
+
+bool
+receiver_init(struct receiver *receiver, socket_t control_socket);
+
+void
+receiver_destroy(struct receiver *receiver);
+
+bool
+receiver_start(struct receiver *receiver);
+
+// no receiver_stop(), it will automatically stop on control_socket shutdown
+
+void
+receiver_join(struct receiver *receiver);
+
+#endif //ANDROID_IROBOT_RECEIVER_HPP
