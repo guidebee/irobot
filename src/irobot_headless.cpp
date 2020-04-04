d778e63148e3795f36bcc154cdfa49933f543667
James Shen <james.shen@guidebee.com>
2020-04-04 21:07:09 +0800

    fix linux complier error

--- src/irobot_headless.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by James Shen on 22/3/20.
-// Copyright (c) 2020 GUIDEBEE IT. All rights reserved
-
-#include "core/irobot_core.hpp"
-
-int main(int argc, char *argv[]) {
-    return irobot::IRobotCore::iRobotMain(argc, argv);
-}
