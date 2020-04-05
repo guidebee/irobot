f81b7c66366d9f214103c714264c1da4139b607f
James Shen <james.shen@guidebee.com>
2020-04-05 21:23:21 +0800

    add json and opencv library

---
+++ tests/test_json.cpp
@@ -0,0 +1,16 @@
+//
+// Created by James Shen on 5/4/20.
+// Copyright (c) 2020 GUIDEBEE IT. All rights reserved
+//
+
+#include "catch.hpp"
+#include <nlohmann/json.hpp>
+using nlohmann::json;
+
+TEST_CASE("test json library", "[json]") {
+    json::string_t s = R"(["foo",1,2,3,false,{"one":1}])";
+    json j = json::parse(s);
+    REQUIRE(json::accept(s));
+    REQUIRE(j == json({"foo", 1, 2, 3, false, {{"one", 1}}}));
+
+}
---
+++ tests/test_opencv.cpp
@@ -0,0 +1,19 @@
+//
+// Created by James Shen on 5/4/20.
+// Copyright (c) 2020 GUIDEBEE IT. All rights reserved
+//
+
+#include "catch.hpp"
+
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+using namespace cv;
+
+TEST_CASE("test opencv library", "[opencv]") {
+    Mat A = Mat(3, 4, CV_32FC1);
+    Mat B = Mat(4, 3, CV_32FC1);
+    REQUIRE(A.rows==3);
+    REQUIRE(B.rows==4);
+}
