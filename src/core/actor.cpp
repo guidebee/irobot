1b000b9d94ed9bb2c396c965d3e1149204f6c03b
James Shen <james.shen@guidebee.com>
2020-04-09 09:42:33 +0800

    add base class actor.cpp

---
+++ src/core/actor.cpp
@@ -0,0 +1,42 @@
+//
+// Created by James Shen on 9/4/20.
+// Copyright (c) 2020 GUIDEBEE IT. All rights reserved
+//
+
+#include "actor.hpp"
+#include "util/lock.hpp"
+
+namespace irobot {
+
+    bool Actor::Init() {
+
+        if (!(this->mutex = SDL_CreateMutex())) {
+            return false;
+        }
+        if (!(this->msg_cond = SDL_CreateCond())) {
+            SDL_DestroyMutex(this->mutex);
+            return false;
+        }
+        this->stopped = false;
+        return true;
+    }
+
+
+    void Actor::Destroy() {
+        SDL_DestroyCond(this->msg_cond);
+        SDL_DestroyMutex(this->mutex);
+
+    }
+
+    void Actor::Stop() {
+        util::mutex_lock(this->mutex);
+        this->stopped = true;
+        util::cond_signal(this->msg_cond);
+        util::mutex_unlock(this->mutex);
+    }
+
+    void Actor::Join() {
+        SDL_WaitThread(this->thread, nullptr);
+
+    }
+}
---
+++ src/core/actor.hpp
@@ -0,0 +1,41 @@
+//
+// Created by James Shen on 9/4/20.
+// Copyright (c) 2020 GUIDEBEE IT. All rights reserved
+//
+
+#ifndef ANDROID_IROBOT_ACTOR_HPP
+#define ANDROID_IROBOT_ACTOR_HPP
+
+#if defined (__cplusplus)
+extern "C" {
+#endif
+
+#include <SDL2/SDL_mutex.h>
+#include <SDL2/SDL_thread.h>
+
+#if defined (__cplusplus)
+}
+#endif
+
+namespace irobot {
+
+    class Actor {
+        SDL_Thread *thread;
+        SDL_mutex *mutex;
+        SDL_cond *msg_cond;
+        bool stopped;
+
+        virtual bool Init();
+
+        virtual void Destroy();
+
+        virtual bool Start() = 0;
+
+        virtual void Stop();
+
+        virtual void Join();
+
+    };
+
+}
+#endif //ANDROID_IROBOT_ACTOR_HPP
